edd58b9827762616e42bdf38be9ef323db3dff5d
Gabriel <gabriel@localhost.localdomain>
2026-01-29 21:21:29 +0800

    added Pyside6/QTPython into the project but needs some adjustment

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+from PySide6 import QtGui, QtWidgets
 import pygame
 import sys
 
-# constants
+# NOTE: constants, will put them into their own file
+# NOTE: for width and height I will use a library to get the screen width and height
 WIDTH = 360
 HEIGHT = 480
 FPS = 30
@@ -11,6 +13,28 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# NOTE: I think its better to put the QT6 thing here, before initialising pygame, lets see
+class ImageWidget(QtWidgets.QWidget):
+    def __init__(self, surface, parent=None) -> None:
+        super(ImageWidget, self).__init__(parent)
+        width = surface.get_width()
+        height = surface.get_height()
+        self.data = surface.get_buffer().raw
+        self.image =QtGui.QImage(self.data, width, height, QtGui.QImage.Format.Format_RGB32) 
+
+    def paintEvent(self, event) -> None:
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.drawImage(0,0,self.image)
+        qp.end()
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, surface, parent=None) -> None:
+        super(MainWindow, self).__init__(parent)
+        self.setCentralWidget(ImageWidget(surface))
+
 pygame.init()
 
 
@@ -32,10 +56,12 @@ class Mate(pygame.sprite.Sprite):
 all_sprites = pygame.sprite.Group()
 mate = Mate()
 all_sprites.add(mate)
-
+app = QtWidgets.QApplication(sys.argv)
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+using_surface = MainWindow(window)
 pygame.display.set_caption("Deskmate made by EskimoGabe")
 clock = pygame.time.Clock()
+
 if __name__ == "__main__":
     while True:
         all_sprites.update()
@@ -45,5 +71,6 @@ if __name__ == "__main__":
                 pygame.quit()
                 sys.exit()
         window.fill(BLACK)
-        all_sprites.draw(window)
-        pygame.display.flip()
+        all_sprites.draw(window) 
+        using_surface.show()
+        app.exec()
