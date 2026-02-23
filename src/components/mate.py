a6fdddb8f21a86346caa8422bc40dce8156a4bf7
EskimoGabe69 <ieueujekekd@gmail.com>
2026-02-23 15:40:15 +0800

    Added: New sprite

--- src/components/mate.py
+++ src/components/mate.py
@@ -1,13 +1,20 @@
 import pygame
 import core.constants as constants
+import os
 
+game_directory = os.path.dirname(__file__)
+parent_directory = os.path.abspath(os.path.join(game_directory, ".."))
+asset_directory = os.path.join(parent_directory, "assets")
+image_file = pygame.image.load(os.path.join(asset_directory, "Doggo.png")).convert_alpha()
+
+# NOTE: The sprite needs to be moved to main I think
 
 class Mate(pygame.sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
         self.image = pygame.Surface((50, 50))
-        self.image.fill((constants.RED))
         self.rect = self.image.get_rect()
+        self.image.blit(image_file, (0, 0))
         self.rect.center = (constants.WIDTH // 2, constants.HEIGHT // 2)
         self.vx = 2
 
@@ -15,4 +22,3 @@ class Mate(pygame.sprite.Sprite):
         self.rect.x += self.vx
         if self.rect.right > constants.WIDTH or self.rect.left < 0:
             self.vx *= -1
-
