072b42d37e05588bafa835113d34c58fedcd5c06
EskimoGabe69 <ieueujekekd@gmail.com>
2026-04-24 20:14:17 +0800

    Updated: fixed widget being centered now; Added: Updaterbutton to the GUI

--- src/components/menuwindow.py
+++ src/components/menuwindow.py
@@ -2,6 +2,8 @@ from core import constants
 from PySide6 import QtCore, QtWidgets, QtGui
 import os
 
+from utils.updater import updater
+
 
 css_path = os.path.join(os.path.dirname(__file__), "..", "assets", "styles.css")
 
@@ -10,17 +12,26 @@ class MenuWindow(QtWidgets.QMainWindow):
     def __init__(self, app, parent=None) -> None:
         super(MenuWindow, self).__init__(parent)
         self.app = app
+        self.setWindowFlags(
+            self.windowFlags() | QtGui.Qt.WindowType.WindowStaysOnTopHint
+        )
         self.css_file = QtCore.QFile(css_path)
         self.css_file.open(QtCore.QFile.ReadOnly)
         self.style_sheet = str(self.css_file.readAll(), encoding="utf-8")
         self.css_file.close()
         self.setWindowTitle(constants.CAPTION)
         grid = QtWidgets.QGridLayout()
-        self.button = QtWidgets.QPushButton("Test button", self)
-        self.button.clicked.connect(self._button_clicked)
+        self.button = QtWidgets.QPushButton("Update button", self)
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(grid)
+        self.setCentralWidget(central_widget)
+        self.button.clicked.connect(self.update_button)
         grid.addWidget(self.button, 0, 0, QtGui.Qt.AlignmentFlag.AlignCenter)
         self.setLayout(grid)
         self.setStyleSheet(self.style_sheet)
 
-    def _button_clicked(self) -> None:
-        print("Test button hello world")
+    # def _button_clicked(self) -> None:
+    #     print("Test button hello world")
+
+    def update_button(self) -> None:
+        updater()
